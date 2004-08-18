80bb363b80bb85b8f2a2d2a197e8786108bca16d
flepied <flepied@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-08-18 02:56:02 +0000

    put session file in /tmp/dbus-$USER-xsession$CURR_DISPLAY
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/s2u/trunk@179788 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- s2u.sh
+++ s2u.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 #---------------------------------------------------------------
 # Project         : Mandrakelinux
-# Module          : alert_applet-0.13
+# Module          : s2u
 # File            : s2u.sh
 # Version         : $Id$
 # Author          : Frederic Lepied
 # Created On      : Sat Jul 31 01:03:37 2004
-# Purpose         : 
+# Purpose         : launch the dbus client attaching it to an
+#             exisiting dbus session or launching a new one to
+#             attache itself to.
 #---------------------------------------------------------------
+
+f="/tmp/dbus-$USER-xsession$CURR_DISPLAY"
 
 vars() {
     CURR_DISPLAY=`LC_ALL=C xdpyinfo | grep 'display' | awk '{print $4}'`
 
-    if [ -f "/var/tmp/dbus-xsession$CURR_DISPLAY" -a -O "/var/tmp/dbus-xsession$CURR_DISPLAY" ]; then
-	. "/var/tmp/dbus-xsession$CURR_DISPLAY"
+    if [ -f "$f" -a -O "$f" ]; then
+	. "$f"
     fi
 }
 
 vars
 
 if [ -z "$DBUS_SESSION_BUS_PID" -o ! -d "/proc/$DBUS_SESSION_BUS_PID" ]; then
-    F=`mktemp /var/tmp/dbus.XXXXXX`
+    F=`mktemp /tmp/dbus.XXXXXX`
     chmod 600 $F
     dbus-launch --sh-syntax --exit-with-session > $F
-    rm -f "/var/tmp/dbus-xsession$CURR_DISPLAY"
-    mv -f $F "/var/tmp/dbus-xsession$CURR_DISPLAY"
+    rm -f "$f"
+    mv -f $F "$f"
     vars
 fi
 
-s2u --daemon=yes
-
+exec s2u --daemon=yes
 
 # s2u.sh ends here
